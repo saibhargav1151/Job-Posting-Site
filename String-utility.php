ccecd6553fd63318a2182febf62ad0ced4358fdb
saibhargav1151 <42796191+saibhargav1151@users.noreply.github.com>
2024-02-18 09:48:37 -0500

    Update String-utility.php

--- String-utility.php
+++ String-utility.php
@@ -1,16 +1,1 @@
-<?php
-class StringUtility{
-    public static function shout($string){
-        return strtoupper($string).'!';
-    }
 
-
-    public static function whisper($string){
-        return strtolower($string).'.';
-    }
-}
-
-
-echo StringUtility::shout('sai bhargav');
-
-?>
